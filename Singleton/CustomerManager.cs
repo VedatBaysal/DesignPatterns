42bb4603ace6ec4c23eee1fcdd8bc1be2f58cda1
VedatBaysal <vedatbaysall@gmail.com>
2020-10-17 18:04:38 +0300

    Add project files.

---
+++ Singleton/CustomerManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Singleton
+{
+    class CustomerManager
+    {
+        private static CustomerManager _customerManager;
+        private static object _lockObject = new object();
+        private CustomerManager()
+        {
+
+        }
+        public static CustomerManager CreateAsSingleton()
+        {
+            lock(_lockObject)
+            {
+                if (_customerManager == null)
+                {
+                    _customerManager = new CustomerManager();
+                }
+                return _customerManager;
+            }
+        }
+        public void Save()
+        {
+            Console.WriteLine("Save");
+        }
+    }
+}
