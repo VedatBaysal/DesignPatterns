73f6c639dca338f4facdd5805ad22184a030eac1
VedatBaysal <vedatbaysall@gmail.com>
2020-10-29 17:55:32 +0300

    Added Prototype

---
+++ Prototype/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Vedat",
+                LastName = "Baysal",
+                City = "İstanbul"
+            };
+
+            Customer customer1 = (Customer)customer.Clone();
+            customer1.FirstName = "Mahmut";
+
+            Console.WriteLine(customer.FirstName);
+
+            Console.WriteLine(customer1.FirstName);
+            Console.ReadKey();
+
+        }
+    }
+}
